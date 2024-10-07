444dcd6a64b212fe5fdb7ee5d4d67cdb94b383cc
glebmustafaev <glebmustafaev87@gmail.com>
2024-10-07 11:56:54 +0300

    Update 3.3.py

--- 3.3.py
+++ 3.3.py
@@ -2,10 +2,12 @@ def print_params(a=1, b='строка', c=True):
     print(a, b, c)
 
 
-print_params(b=25, c=[1, 2, 3])
+print_params()
+print_params(b=25)
+print_params(c=[1, 2, 3])
 value_list = [1, 'list', False]
 value_dict = {'a': 1, 'b': 'строка', 'c': True}
 print_params(*value_list)
 print_params(**value_dict)
-value_list_2 = [54.32,True]
-print_params(*value_list_2,42)
+value_list_2 = [54.32, True]
+print_params(*value_list_2, 42)
