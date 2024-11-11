afeacbcd731ad75616ecb0ed19eee25b939e4e7f
glebmustafaev <glebmustafaev87@gmail.com>
2024-11-11 16:18:17 +0300

    Update and rename 5.hard to 5.hard.py

--- 5.hard.py
+++ 5.hard.py
@@ -1,7 +1,7 @@
 class User:
-    def __init__(self,nickname: str,password: int,age: int):
+    def __init__(self,nickname: str,hashpass: int,age: int):
         self.nickname = nickname
-        self.password = password
+        self.password = hashpass
         self.age = age
 class Video:
     def __init__(self,title: str,duration: int,time_now,adult_mode: bool = False):
@@ -19,9 +19,18 @@ class UrTube:
             if nickname == user.nickname and password == user.password:
                 self.current_user = user
     def register(self,nickname: str, password: str, age: int):
+        hashpass = hash(password)
         for user in self.users:
             if user.nickname == nickname:
                 print(f"Пользователь {nickname} уже существует")
             else: self.users.append(nickname)
     def log_out(self):
         self.current_user = None
+    def add(self,*args):
+        for video in args:
+            if video not in self.videos:
+                self.videos.append(video)
+    def get_videos(self,word: str):
+        list.videoS = []
+        for video in self.videos:
+            if word.lower() in 
