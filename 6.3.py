bb94f28bb59da33f23ce239609c264860eb31749
glebmustafaev <glebmustafaev87@gmail.com>
2024-11-13 14:19:54 +0300

    Update 6.3.py

--- 6.3.py
+++ 6.3.py
@@ -5,12 +5,17 @@ class Animal:
     live = True
     sound = None
     _DEGREE_OF_DANGER = 0
+    x = 0
+    y = 0
+    z = 0
+    _cords = [x, y, z]
 
+    def __init__(self, speed):
 
-    def __init__(self, speed: int, _cords: list[0, 0, 0]):
-        self._cords = _cords
         self.speed = speed
 
+    def speak(self):
+        print(self.sound)
 
     def move(self, dx, dy, dz):
         self._cords[0] += dx * self.speed
@@ -19,32 +24,38 @@ class Animal:
         if dz < 0:
             print("It's too deep, i can't dive :(")
         return self._cords
+
     def get_cords(self):
         print(f"X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}")
 
-    def attack(self, _DEGREE_OF_DANGER):
-        if _DEGREE_OF_DANGER < 5:
+    def attack(self):
+        if self._DEGREE_OF_DANGER < 5:
             print("Sorry, i'm peaceful :)")
         else:
             print("Be careful, i'm attacking you 0_0")
 
 
+def lay_eggs():
+    print(f"Here are(is) {random.randint(1, 4)} eggs for you")
+
+
 class Bird(Animal):
     beak = True
-
-    def lay_eggs(self):
-        print(f"Here are(is) {random.randint(1, 4)} eggs for you")
 
 
 class AquaticAnimal(Animal):
     _DEGREE_OF_DANGER = 3
 
     def dive_in(self, dz):
-         self._cords[2] = dz * (-1/2) * self.speed
+        Animal.z=(Animal.z/2)-abs(dz)
+
+
+
 class PoisonousAnimal(Animal):
     _DEGREE_OF_DANGER = 8
-class Duckbill(Bird,AquaticAnimal,PoisonousAnimal):
 
+
+class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
     sound = "Click-click-click"
 
 
@@ -61,4 +72,5 @@ db.get_cords()
 db.dive_in(6)
 db.get_cords()
 
-db.lay_eggs()
+lay_eggs()
+
