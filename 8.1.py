88aea02d16bb9dc61dda44bfc2b412da968b3de2
glebmustafaev <glebmustafaev87@gmail.com>
2024-11-20 17:04:08 +0300

    Update 8.1.py

--- 8.1.py
+++ 8.1.py
@@ -1,9 +1,8 @@
 def add_everything_up(a,b):
     try:
-        if a(int,float) and b(int,float):
-            return a + b
+        return a + b
     except TypeError:
-        return f'{a(str)}{b(str)}'
+        return f'{str(a)}{str(b)}'
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
