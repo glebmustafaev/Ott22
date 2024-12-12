d37b4a7fc14eb64e49473c46e7ea93a5227a4218
glebmustafaev <glebmustafaev87@gmail.com>
2024-12-12 14:18:33 +0300

    Create 10.2.py

---
+++ 10.2.py
@@ -0,0 +1,29 @@
+import threading
+import time
+
+
+class Knight(threading.Thread):
+    def __init__(self, name: str, power: int):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.power = power
+
+    def run(self):
+        print(f'{self.name} на нас напали')
+        wargs = 100
+        x = 0
+        while wargs > 0:
+            time.sleep(1)
+            print(f'{self.name} сражается {x} день, осталось {wargs} врагов')
+            wargs -= self.power
+            x += 1
+            if wargs == 0:
+                print(f'{self.name} одержал победу на {x} день')
+
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+first_knight.start()
+second_knight.start()
+first_knight.join()
+second_knight.join()
