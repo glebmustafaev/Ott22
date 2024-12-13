887b113871ba3f2a59a9dfc3cc9ec3186cd66b60
glebmustafaev <glebmustafaev87@gmail.com>
2024-12-13 19:17:58 +0300

    Update 10.3.py

--- 10.3.py
+++ 10.3.py
@@ -1,44 +1,41 @@
 import threading
+from threading import Thread, Lock
 import random
 import time
 
-lock = threading.Lock()
 
 
-class Bank(threading.Thread):
+class Bank(Thread):
 
-    def __init__(self, balance: int):
-        threading.Thread.__init__(self)
-        self.balance = balance
-        self.lock = lock
+    def __init__(self):
+        self.balance = 0
+        self.lock = Lock()
+        super().__init__()
 
     def deposit(self):
-        n = 0
-
-        while n < 3:
-            num = random.randint(50, 500)
-            self.balance = self.balance + num
-            if self.balance >= 500 or lock.locked():
-                lock.release()
-            print(f"Пополнение: {num}. Баланс: {self.balance}")
-            n += 1
-        time.sleep(0.001)
+        for i in range(100):
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            y = random.randint(50, 500)
+            self.balance += y
+            print(f'Пополнение: {y}. Баланс: {self.balance}')
+            time.sleep(0.001)
 
     def take(self):
-        n = 0
-        while n < 3:
-            num_ = random.randint(50, 500)
-            print(f'Запрос на {num_}')
-            if num_ <= self.balance:
-                self.balance = self.balance - num_
-                print(f"Снятие: {num_}. Баланс: {self.balance}")
+        for i in range(100):
+            j = random.randint(50, 500)
+            print(f'Запрос на {j}')
+            if j <= self.balance:
+                self.balance -= j
+                print(f"Снятие: {j}. Баланс: {self.balance}")
             else:
                 print("Запрос отклонён, недостаточно средств")
-                lock.acquire()
-            n += 1
+                self.lock.acquire()
+            time.sleep(0.001)
 
 
-bk = Bank(0)
+
+bk = Bank()
 th1 = threading.Thread(target=Bank.deposit, args=(bk,))
 th2 = threading.Thread(target=Bank.take, args=(bk,))
 th1.start()
