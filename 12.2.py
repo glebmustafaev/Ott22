c9483816a77a296039914f6e5f7771472bb31732
glebmustafaev <glebmustafaev87@gmail.com>
2025-01-20 20:46:57 +0300

    Update 12.2.py

--- 12.2.py
+++ 12.2.py
@@ -1,13 +1,14 @@
-import unittest
 import runner
+import unittest
+
 class TournamentTest(unittest.TestCase):
     def setUpClass(self):
         self.all_results = None
 
     def setUp(self):
-        r_1 = runner('Усэйн',10)
-        r_2 = runner('Андрей',9)
-        r_3 = runner('Ник',3)
+        r_1 = runner.Runner('Усэйн',10)
+        r_2 = runner.Runner('Андрей',9)
+        r_3 = runner.Runner('Ник',3)
 
     def tearDownClass(self):
         print(self.all_results, end='\n')
