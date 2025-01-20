a2dc32c835f30d5a5f5697eeb2890c26f74861e7
glebmustafaev <glebmustafaev87@gmail.com>
2025-01-20 22:36:26 +0300

    Update 12.2.py

--- 12.2.py
+++ 12.2.py
@@ -1,14 +1,38 @@
 import runner
 import unittest
 
+
 class TournamentTest(unittest.TestCase):
-    def setUpClass(self):
-        self.all_results = None
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
 
     def setUp(self):
-        r_1 = runner.Runner('Усэйн',10)
-        r_2 = runner.Runner('Андрей',9)
-        r_3 = runner.Runner('Ник',3)
+        self.runner1 = runner.Runner('Усейн', 10)
+        self.runner2 = runner.Runner('Андрей', 9)
+        self.runner3 = runner.Runner('Ник', 3)
 
-    def tearDownClass(self):
-        print(self.all_results, end='\n')
+    @classmethod
+    def tearDownClass(cls):
+        for test_key, test_value in cls.all_results.items():
+            print(f'Тест: {test_key}')
+            for key, value in test_value.items():
+                print(f'\t{key}: {value.name}')
+
+    def test_1(self):
+        obj_1 = runner.Tournament(90, self.runner1, self.runner3)
+        result = obj_1.start()
+        self.assertTrue(list(result.values())[-1] == self.runner3)
+        self.all_results[f'Результат {self.runner1} и {self.runner3}'] = result
+
+    def test_2(self):
+        obj_2 = runner.Tournament(90, self.runner2, self.runner3)
+        result = obj_2.start()
+        self.assertTrue(list(result.values())[-1] == self.runner3)
+        self.all_results[f'Результат {self.runner2} и {self.runner3}'] = result
+
+    def test_3(self):
+        obj_3 = runner.Tournament(90, self.runner1,self.runner2, self.runner3)
+        result = obj_3.start()
+        self.assertTrue(list(result.values())[-1] == self.runner3)
+        self.all_results[f'Результат {self.runner1} и {self.runner1} и {self.runner3}'] = result
